571278e5fcc374cf38c0c301f45a46f4907d49f9
YISHTY <1252537277@qq.com>
2022-03-25 14:11:55 +0800

    new

--- src/main/java/com/example/demo/generate/UserGenerator.java
+++ src/main/java/com/example/demo/generate/UserGenerator.java
@@ -11,11 +11,11 @@ import java.util.List;
 
 public class UserGenerator {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://192.168.193.235:3306/demo?useUnicode=true&characterEncoding=utf8&useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=GMT%2B8";
+        String url = "jdbc:mysql://192.168.193.235:3306/demo?characterEncoding=utf-8&useSSL=false&useTimezone=true&serverTimezone=GMT%2B8";
         List<String> tables = new ArrayList<>();
         tables.add("user");
 
-        FastAutoGenerator.create(url, "root", "root")
+        FastAutoGenerator.create(url, "root", "admin@123")
                 .globalConfig(builder -> {
                     builder.author("yzk")               //作者
                             .outputDir(System.getProperty("user.dir") + "\\src\\main\\java")    //输出路径(写到java目录)
@@ -24,37 +24,33 @@ public class UserGenerator {
                             .fileOverride();            //开启覆盖之前生成的文件
 
                 })
-                .packageConfig(builder -> {
-                    builder.parent("com.example.demo")
-                            .moduleName("practice")
-                            .entity("entity")
-                            .service("service")
-                            .serviceImpl("serviceImpl")
-                            .controller("controller")
-                            .mapper("mapper")
-                            .xml("mapper")
-                            .pathInfo(Collections.singletonMap(OutputFile.xml, System.getProperty("user.dir") + "\\src\\main\\resources\\mapper"));
-                })
-                .strategyConfig(builder -> {
-                    builder.addInclude(tables)
-                            .addTablePrefix("p_")
-                            .serviceBuilder()
-                            .formatServiceFileName("%sService")
-                            .formatServiceImplFileName("%sServiceImpl")
-                            .entityBuilder()
-                            .enableLombok()
-                            .logicDeleteColumnName("deleted")
-                            .enableTableFieldAnnotation()
-                            .controllerBuilder()
-                            .formatFileName("%sController")
-                            .enableRestStyle()
-                            .mapperBuilder()
-                            .enableBaseResultMap()  //生成通用的resultMap
-                            .superClass(BaseMapper.class)
-                            .formatMapperFileName("%sMapper")
-                            .enableMapperAnnotation()
-                            .formatXmlFileName("%sMapper");
-                })
+                .packageConfig(builder -> builder.parent("com.example.demo")
+                        .moduleName("practice")
+                        .entity("entity")
+                        .service("service")
+                        .serviceImpl("serviceImpl")
+                        .controller("controller")
+                        .mapper("mapper")
+                        .xml("mapper")
+                        .pathInfo(Collections.singletonMap(OutputFile.xml, System.getProperty("user.dir") + "\\src\\main\\resources\\mapper")))
+                .strategyConfig(builder -> builder.addInclude(tables)
+                        .addTablePrefix("p_")
+                        .serviceBuilder()
+                        .formatServiceFileName("%sService")
+                        .formatServiceImplFileName("%sServiceImpl")
+                        .entityBuilder()
+                        .enableLombok()
+                        .logicDeleteColumnName("deleted")
+                        .enableTableFieldAnnotation()
+                        .controllerBuilder()
+                        .formatFileName("%sController")
+                        .enableRestStyle()
+                        .mapperBuilder()
+                        .enableBaseResultMap()  //生成通用的resultMap
+                        .superClass(BaseMapper.class)
+                        .formatMapperFileName("%sMapper")
+                        .enableMapperAnnotation()
+                        .formatXmlFileName("%sMapper"))
                 .templateEngine(new FreemarkerTemplateEngine())   // 使用Freemarker引擎模板，默认的是Velocity引擎模板
                 .execute();
     }
